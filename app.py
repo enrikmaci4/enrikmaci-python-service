958e0174d74d000e27a9a794cfebc6d5721a884b
Enrik Maci <enrik.maci@metaswitch.com>
2023-12-03 14:18:57 +0000

    Added functionality to show a random question

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import em.q as q
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
@@ -18,14 +23,15 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    name = request.form.get('name')
+    name = q.generate()
+    
+    if name:
+        print('%s' % name)
+        return render_template('hello.html', name = name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
